d5e36bdfd43ace2ca690d6a7f208974ddd3715e8
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-03-23 22:40:40 +1100

    feat: improved rendering of callback return values

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -5,6 +5,8 @@ function isEventKey(key: string): key is (typeof events)[number] {
   return events.includes(key as any);
 }
 
+type _Element = Element;
+
 declare global {
   module JSX {
     type IntrinsicElements = {
@@ -17,14 +19,13 @@ declare global {
     };
 
     type Element = {
-      node: Node;
+      node: _Element;
     };
   }
 }
 
 type AnyProps = Record<string, unknown>;
 type IntrinsicProps = JSX.IntrinsicElements[keyof JSX.IntrinsicElements];
-type Children = (null | JSX.Element | (() => null | JSX.Element))[];
 
 export type Component = () => JSX.Element;
 
@@ -33,67 +34,104 @@ namespace JSXArgs {
   export type Intrinsic = [
     tag: string,
     props: IntrinsicProps | null,
-    ...children: Children,
+    ...children: unknown[],
   ];
   export type Function = [
     tag: (props: AnyProps) => JSX.Element,
     props: AnyProps | null,
-    ...children: Children,
+    ...children: unknown[],
   ];
 }
 
 export const jsx = (...args: JSXArgs): JSX.Element => {
   console.debug("jsx", args);
 
-  let node: Node;
+  let parent: Element;
   const [, , ...children] = args;
   if (typeof args[0] === "string") {
     const [tag, props] = args as JSXArgs.Intrinsic;
-    node = document.createElement(tag);
+    parent = document.createElement(tag);
     if (props) {
       for (const key in props) {
         if (isEventKey(key)) {
           const type = key.substring(2);
           const callback = props[key];
           if (callback) {
-            node.addEventListener(type, callback as any);
+            parent.addEventListener(type, callback as any);
           }
+        } else if (key === "for") {
+          // @ts-ignore TODO: Fix this.
+          parent.htmlFor = props[key];
+        } else {
+          // @ts-ignore TODO: Fix this.
+          parent[key] = props[key];
         }
       }
     }
   } else {
     const [tag, props] = args as JSXArgs.Function;
-    node = tag(props ?? {}).node;
+    parent = tag(props ?? {}).node;
   }
 
-  const nodeChildren: (string | Node)[] = [];
   children.forEach((child, index) => {
-    if (child === null) {
+    if (child === undefined) {
       return;
-    }
-    if (typeof child === "object") {
-      nodeChildren.push(child.node);
+    } else if (child === null) {
+      return;
+    } else if (typeof child === "boolean") {
+      return;
+    } else if (typeof child === "number") {
+      parent.append(child.toString());
+    } else if (typeof child === "string") {
+      parent.append(child);
+    } else if (
+      typeof child === "object" &&
+      "node" in child &&
+      child.node instanceof Node
+    ) {
+      parent.append(child.node);
     } else if (typeof child === "function") {
-      const render = () => `${child()}`;
-      const { result, atoms } = listen(() => render());
+      const { result, atoms } = listen(() => child() as unknown);
+
       atoms.forEach((atom) =>
         atom.callbacks.push(() => {
-          const child = node.childNodes[index];
-          if (child) {
-            child.textContent = render();
+          const resultFormatted = format(child());
+          const currentNode = parent.childNodes[index]!;
+          if (resultFormatted) {
+            currentNode.replaceWith(resultFormatted);
+          } else {
+            currentNode.remove();
           }
         }),
       );
-      nodeChildren.push(result);
-    } else {
-      nodeChildren.push(child);
+
+      const resultFormatted = format(result);
+      if (resultFormatted) {
+        parent.append(resultFormatted);
+      }
     }
   });
-  if (node instanceof Element && nodeChildren.length) {
-    node.append(...nodeChildren);
-  }
 
-  return {
-    node,
-  };
+  return { node: parent };
 };
+
+function format(value: unknown): undefined | string | Node {
+  if (value === undefined) {
+    return undefined;
+  } else if (value === null) {
+    return undefined;
+  } else if (typeof value === "boolean") {
+    return undefined;
+  } else if (typeof value === "number") {
+    return value.toString();
+  } else if (typeof value === "string") {
+    return value;
+  } else if (
+    typeof value === "object" &&
+    "node" in value &&
+    value.node instanceof Node
+  ) {
+    return value.node;
+  }
+  return undefined;
+}
