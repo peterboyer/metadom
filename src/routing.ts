d47890f559cfe8b084a20e16a9a34868aa7a50fe
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-04-01 12:52:05 +1100

    feat: routing.* history methods

--- src/routing.ts
+++ src/routing.ts
@@ -2,7 +2,30 @@ import { Signal } from "./signal";
 
 export const url = Signal(new URL(document.URL));
 
+export function go(delta: number = 0): void {
+	history.go(delta);
+	url(new URL(document.URL));
+}
+
+export function forward(): void {
+	history.forward();
+	url(new URL(document.URL));
+}
+
+export function back(): void {
+	history.back();
+	url(new URL(document.URL));
+}
+
 export function push(nextUrl: string, state?: Record<string, unknown>): void {
 	history.pushState(state, "", nextUrl);
-	url(new URL(nextUrl));
+	url(new URL(document.URL));
+}
+
+export function replace(
+	nextUrl: string,
+	state?: Record<string, unknown>,
+): void {
+	history.replaceState(state, "", nextUrl);
+	url(new URL(document.URL));
 }
