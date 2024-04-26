3ab2f15f597d82122433bb3ef559698e8aa65d1c
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-04-26 22:18:29 +1000

    fix: include README.md in dist

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ set -euo pipefail
 [[ -d dist ]] && rm -r dist
 tsc --project tsconfig.build.json
 cp src/config/tsconfig.json dist/config
-cp package.json dist
+cp package.json README.md dist
 cd dist
 	npm pkg delete scripts
 	npm pkg delete devDependencies
-	npm pkg delete tsup
 	npm pkg delete prettier
